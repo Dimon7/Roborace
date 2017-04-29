eb24bf40a8fccda04c81d02c6fbe1557ab96b490
Dimon7 <savchuk.dsma@yandex.ua>
2017-04-29 15:40:51 +0300

    can turn and accelarate

--- led/led.ino
+++ led/led.ino
@@ -1,57 +1,72 @@
 #include <Servo.h>
 
+Servo TurnServo;
+Servo AccelerateServo;
+int val;
 
-
-Servo servo;
-
- 
+boolean flag = false;
 void setup()
 {
   Serial.begin(9600);
-      
 
-  servo.attach(10);
+  pinMode(16, OUTPUT);
 
-  servo.write(86);  
+  AccelerateServo.attach(16);
+  AccelerateServo.write(100);// 90 stop  
+
+  TurnServo.attach(10);//angle
+  TurnServo.write(86);  
 }
 
 void loop()                    
 {
-  
-  switch( Serial.read() ){
-    
-    case 'r' : { 
-                //right(); 
-                servo.write(55);
-                break;
-    }
-    case 'l' : { 
-                //left();
-                servo.write(115);
-                break;  
-    }
-    case 'o' : { 
-//                none(); 
-              servo.write(86);
-                break;
-    }
-      
-  }
+  //  digitalWrite(16, HIGH);
+  //  if(!flag){
+  //  
+  //    TurnServo.attach(10);//angle 
+  //    val = 135;
+  //    TurnServo.write(val); //Move the servo (0 degree)
+  //    delay(2000);
+  //    TurnServo.detach();
+  //    flag = true;
+  //  }
 
-//
-//  void none(){
-//
-//    servo.write(86);  
-//  }
-//  void left(){
-//      servo.write(115);  
-//  }
-//
-//  void right(){
-//    servo.write(55);
-//    
-//  }
-    
-  
-         
+
+  //  switch( Serial.read() ){
+  //    
+  //    case 'r' : { 
+  //                //right(); 
+  //                servo.write(55);
+  //                break;
+  //    }
+  //    case 'l' : { 
+  //                //left();
+  //                servo.write(115);
+  //                break;  
+  //    }
+  //    case 'o' : { 
+  ////                none(); 
+  //              servo.write(86);
+  //                break;
+  //    }
+  //      
+
+
+  //
+  //  void none(){
+  //
+  //    servo.write(86);  
+  //  }
+  //  void left(){
+  //      servo.write(115);  
+  //  }
+  //
+  //  void right(){
+  //    servo.write(55);
+  //    
+  //  }
+
+
+
 }
+
