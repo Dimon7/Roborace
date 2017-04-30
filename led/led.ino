59601c18af8ecccc1d03f45b4ef65b55c4f617b8
Dimon7 <savchuk.dsma@yandex.ua>
2017-04-30 18:18:40 +0300

    proba

--- led/led.ino
+++ led/led.ino
@@ -1,72 +1,116 @@
+#include <SerialCommand.h>
+
+#include <SoftwareSerial.h>
+
 #include <Servo.h>
 
-Servo TurnServo;
-Servo AccelerateServo;
-int val;
+  Servo TurnServo;
+  Servo AccelerateServo;
+  SerialCommand sCmd;
+  
+  int stopk = 86;
+  int val;
+  
+  int accelerate = 16; //pin
+  int turn = 10; //pin
 
-boolean flag = false;
 void setup()
 {
   Serial.begin(9600);
-
-  pinMode(16, OUTPUT);
-
-  AccelerateServo.attach(16);
-  AccelerateServo.write(100);// 90 stop  
-
-  TurnServo.attach(10);//angle
+  
+  AccelerateServo.attach(accelerate);
+  TurnServo.attach(turn);
+  
+  AccelerateServo.write(90);// 90 stop  
   TurnServo.write(86);  
+  
+  sCmd.addCommand("r", Go);
+  sCmd.addCommand("a", Ang);
+
+  sCmd.addCommand("W", forward);
+  sCmd.addCommand("D", TurnRight);
+  sCmd.addCommand("A", TurnLeft);
+  sCmd.addCommand("S", back );
+
+//  sCmd.addDefaultHandler(unrecognized);
 }
 
 void loop()                    
 {
-  //  digitalWrite(16, HIGH);
-  //  if(!flag){
-  //  
-  //    TurnServo.attach(10);//angle 
-  //    val = 135;
-  //    TurnServo.write(val); //Move the servo (0 degree)
-  //    delay(2000);
-  //    TurnServo.detach();
-  //    flag = true;
-  //  }
+  sCmd.readSerial();
+}
+void Go() {
+  int speed_ = 0;
+  int speed_val = 88; // 86 90
+
+  char *arg;
+  arg = sCmd.next();
+  if (arg != NULL)      // As long as it existed, take it
+  {
+    Serial.println(arg);
+    speed_ = atoi(arg);
+    if (speed_ < 0) {
+      speed_val = 90 - speed_;
+    } else if (speed_ > 0) {
+      speed_val = 86 - speed_;
+    } else {
+      speed_val = 88;
+    }
+  }
+
+  AccelerateServo.write(speed_val);
+
+}
+
+void Ang() {
+  int ang_ = 3;
+  int ang_val = 84; //
+
+  char *arg;
+  arg = sCmd.next();
+  if (arg != NULL)      // As long as it existed, take it
+  {
+    Serial.println(arg);
+    ang_ = atoi(arg);
+    ang_val = 84 - ang_;
+  }
+
+  TurnServo.write(ang_val);
 
 
-  //  switch( Serial.read() ){
-  //    
-  //    case 'r' : { 
-  //                //right(); 
-  //                servo.write(55);
-  //                break;
-  //    }
-  //    case 'l' : { 
-  //                //left();
-  //                servo.write(115);
-  //                break;  
-  //    }
-  //    case 'o' : { 
-  ////                none(); 
-  //              servo.write(86);
-  //                break;
-  //    }
-  //      
+}
 
 
-  //
-  //  void none(){
-  //
-  //    servo.write(86);  
-  //  }
-  //  void left(){
-  //      servo.write(115);  
-  //  }
-  //
-  //  void right(){
-  //    servo.write(55);
-  //    
-  //  }
+void forward() {
+  TurnServo.write(84);
+  stopk++;
+  Serial.println(stopk);
+  AccelerateServo.write(stopk);
 
+}
+void back() {
+  Serial.println("DOWN");
+  TurnServo.write(92);
+  stopk--;
+  Serial.println(stopk);
+  AccelerateServo.write(stopk);
+}
+void TurnLeft() {
+  Serial.println("left");
+  TurnServo.write(115);
+  //AccelerateServo.write();
+}
+void TurnRight() {
+  Serial.println("right");
+  TurnServo.write(55);
+  //AccelerateServo();
+}
 
-
+void Stop() {
+  Serial.println("stop");
+  AccelerateServo.write(75);
+}
+void unrecognized() {
+  Serial.println("alredy");
 }
 
